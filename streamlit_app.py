c793a651b4e72aeef13a588c07d8d25b6b48c961
nmacosta <ninoska.acosta@gmail.com>
2025-06-25 14:40:18 -0400

    regresando a la version

--- streamlit_app.py
+++ streamlit_app.py
@@ -459,7 +459,7 @@ with col1_params:
 
 with col2_params:
     st.subheader("Modelo IA (para Análisis)")
-    model_options = ['gemini-2.5-pro-preview-05-06','gemini-2.5-flash-lite-preview-06-17']
+    model_options = ['igemini-2.5-pro-exp-03-25','gemini-2.5-flash-lite-preview-06-17']
     selected_model_name = st.selectbox(
         "Modelo Gemini:",
         options=model_options,
